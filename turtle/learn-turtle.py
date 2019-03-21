d50dbc5be5521f89874057cf53b2189a5ad02ab4
Federico Feller <ffeller@despegar.com>
2019-03-21 13:43:01 -0300

    param

--- turtle/learn-turtle.py
+++ turtle/learn-turtle.py
@@ -4,7 +4,6 @@ import shapes
 tess = Turtle()
 tess.shape("turtle")
 
-
-shapes.draw_square(tess)
+shapes.draw_square(tess, 50)
 
 exitonclick()  # Why this? Experiment by commenting it out.
--- turtle/shapes.py
+++ turtle/shapes.py
@@ -1,4 +1,4 @@
-def draw_square(turtle):
+def draw_square(turtle, length=100):
     for i in range(4):
-        turtle.forward(100)
+        turtle.forward(length)
         turtle.left(90)
