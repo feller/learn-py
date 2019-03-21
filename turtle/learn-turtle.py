850e5e140b3b17b904909539a6ef212ae31f40d3
Federico Feller <ffeller@despegar.com>
2019-03-21 14:00:17 -0300

    wip

--- turtle/learn-turtle.py
+++ turtle/learn-turtle.py
@@ -11,4 +11,14 @@ shapes.draw_rectangle(tess, 100, 50)
 tess.color('blue')
 shapes.draw_triangle(tess, 70)
 
+for i in range(3):
+    tess.left(20)
+    shapes.draw_square(tess, 90)
+
+tess.left(180)
+tess.forward(200)
+shapes.draw_rectangle(tess, 100, 50)
+shapes.draw_triangle(tess, 70)
+
+
 exitonclick()  # Why this? Experiment by commenting it out.
